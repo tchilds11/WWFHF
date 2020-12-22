3d49fe1985b3bdf4cdf86bc5afba039dd98faac3
Terrence Childs <tchilds1108@gmail.com>
2020-12-22 10:26:28 -0500

    character

---
+++ character_list.py
@@ -0,0 +1,11 @@
+character1 = Character("Bronzeberg", 50, "high", "high",
+                         "high", "Spear")
+character2 = Character("The Overtaker", 50, "medium", "low",
+                         "medium", "Piledriver")
+character3 = Character("Kurt Angle", 50, "low",
+                       "high", "low", "Leg Lock")
+character4 = Character("The Show", 50, "low",
+                       "high", "low", "Chokeslam")
+
+def character_list():
+    return [character1, character2, character3, character4]
